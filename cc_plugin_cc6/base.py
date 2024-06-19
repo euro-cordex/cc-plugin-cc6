4369dd9ccc4206b3a1e1cf5a4d0076997d2df922
sol1105 <martin.schupfner@web.de>
2024-06-19 17:39:49 +0200

    Updated checks
    - added check for DRS path, DRS file name and suffix
    - generalized reading of CMOR tables

--- cc_plugin_cc6/base.py
+++ cc_plugin_cc6/base.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 from pathlib import Path
 
 from compliance_checker.base import BaseCheck, BaseNCCheck, Result
@@ -28,45 +29,318 @@ class CORDEXCMIP6(BaseNCCheck, CORDEXCIMP6Base):
         return Result(level, (score, out_of), name, messages)
 
     def setup(self, dataset):
+        self.debug = True
         self.dataset = dataset
         self.options = self.options
         # self.ds = xr.open_dataset(dataset.filepath(), decode_times=False)
-        # Read CV and CMOR tables
-        self.CV = self._read_CV("CV")["CV"]
-        self.CT1hr = self._read_CV("1hr")
-        self.CT6hr = self._read_CV("6hr")
-        self.CTday = self._read_CV("day")
-        self.CTmon = self._read_CV("mon")
-        self.CTfx = self._read_CV("fx")
-        self.coords = self._read_CV("coordinate")
-        self.grids = self._read_CV("grids")
-        self.formulas = self._read_CV("formula_terms")
-        varlist = set(
-            list(self.CT1hr["variable_entry"].keys())
-            + list(self.CT6hr["variable_entry"].keys())
-            + list(self.CTday["variable_entry"].keys())
-            + list(self.CTmon["variable_entry"].keys())
-            + list(self.CTfx["variable_entry"].keys())
-        )
-        # Identify variable name (cf_xarray)
-        var_ids = [v for v in varlist if v in list(dataset.variables.keys())]
-        self.varname = var_ids
-
-    def _read_CV(self, table_name):
-        """Reads the specified CV table."""
+        # Get path to the tables
         if self.inputs.get("tables", False):
             cc6cv = self.inputs["tables"]
         else:
-            cc6cv = os.getenv("CORDEXCMIP6TABLESPATH", "./")
-        cc6cv = Path(cc6cv, f"CORDEX-CMIP6_{table_name}.json")
-        # Read CV
+            cc6cv = "./"
+            # Download tables
+            #   to be implemented
+            # Environment variable
+            # cc6cv = os.getenv("CORDEXCMIP6TABLESPATH", "./")
+        # Identify table prefix and table names
+        tables = [
+            t
+            for t in os.listdir(cc6cv)
+            if os.path.isfile(os.path.join(cc6cv, t))
+            and t.endswith(".json")
+            and "example" not in t
+        ]
+        table_prefix = tables[0].split("_")[0]
+        table_names = ["_".join(t.split("_")[1:]).split(".")[0] for t in tables]
+        if not all([table_prefix + "_" + t + ".json" in tables for t in table_names]):
+            raise ValueError(
+                "CMOR tables do not follow the naming convention '<project_id>_<table_id>.json'."
+            )
+        # Read CV and coordinate tables
+        self.CV = self._read_CV(cc6cv, table_prefix, "CV")["CV"]
+        self.coords = self._read_CV(cc6cv, table_prefix, "coordinate")
+        self.grids = self._read_CV(cc6cv, table_prefix, "grids")
+        self.formulas = self._read_CV(cc6cv, table_prefix, "formula_terms")
+        # Read variable tables (variable tables)
+        self.CT = {}
+        for table in table_names:
+            if table in ["CV", "grids", "coordinate", "formula_terms"]:
+                continue
+            self.CT[table] = self._read_CV(cc6cv, table_prefix, table)
+            if "variable_entry" not in self.CT[table]:
+                raise KeyError(
+                    f"CMOR table '{table}' does not contain the key 'variable_entry'."
+                )
+            if "Header" not in self.CT[table]:
+                raise KeyError(
+                    f"CMOR table '{table}' does not contain the key 'Header'."
+                )
+            for key in ["table_id"]:
+                if key not in self.CT[table]["Header"]:
+                    print(table, key)
+                    raise KeyError(
+                        f"CMOR table '{table}' misses the key '{key}' in the header information."
+                    )
+        # Compile varlist for quick reference
+        varlist = list()
+        for table in table_names:
+            if table in ["CV", "grids", "coordinate", "formula_terms"]:
+                continue
+            varlist = varlist + list(self.CT[table]["variable_entry"].keys())
+        varlist = set(varlist)
+        # Map DRS building blocks to the filename, filepath and global attributes
+        self._map_drs_blocks()
+        # Identify variable name(s)
+        var_ids = [v for v in varlist if v in list(dataset.variables.keys())]
+        self.varname = var_ids
+
+    def _read_CV(self, path, table_prefix, table_name):
+        """Reads the specified CV table."""
+        cc6cv = Path(path, f"{table_prefix}_{table_name}.json")
         try:
             with open(cc6cv) as f:
                 return json.load(f)
         except (FileNotFoundError, IsADirectoryError, PermissionError) as e:
             raise Exception(
-                f"Could not find or open '{cc6cv}'. Please make sure the environment variable 'CORDEXCMIP6TABLESPATH' is set and points to the Tables subdirectory of the cloned 'cordex-cmip6-cmor-tables' git repository."
+                f"Could not find or open table '{table_prefix}_{table_name}.json' under path '{path}'."
             ) from e
+
+    def _compare_CV_element(self, el, val):
+        """Compares value of a CV entry to a given value."""
+        # ########################################################################################
+        # 5-6 Types of CV entries ('*' is the element that is the value for comparison):
+        # 0 # value
+        # 1 # key -> *list of values
+        # 2 # key -> *list of length 1 (regex)
+        # 3 # key -> *dict key -> value
+        # 4 # key -> *dict key -> dict key -> *value
+        # 5 # key -> *dict key -> dict key -> *list of values
+        # 6 # key (source_id) -> *dict key -> dict key (license_info) -> dict key (id, license) -> value (CMIP6 only)
+        # ########################################################################################
+        # 0 (2nd+ level comparison) #
+        if isinstance(el, str):
+            if self.debug:
+                print(val, "->0")
+            return bool(re.fullmatch(el, str(val), flags=re.ASCII)), []
+        # 1 and 2 #
+        elif isinstance(el, type(list())):
+            if self.debug:
+                print(val, "->1 and 2")
+            if val not in el:
+                return (
+                    any(
+                        [
+                            bool(re.fullmatch(eli, str(val), flags=re.ASCII))
+                            for eli in el
+                        ]
+                    ),
+                    [],
+                )
+            else:
+                return True, []
+        # 3 to 6 #
+        elif isinstance(el, type(dict())):
+            if self.debug:
+                print(val, "->3 to 6")
+            if val in el.keys():
+                # 3 #
+                if isinstance(el[val], str):
+                    if self.debug:
+                        print(val, "->3")
+                    return True, []
+                # 4 to 6 #
+                elif isinstance(el[val], type(dict())):
+                    if self.debug:
+                        print(val, "->4 to 6")
+                    return True, list(el[val].keys())
+                else:
+                    raise ValueError(
+                        f"Unknown CV structure for element: {el} and value {val}."
+                    )
+            else:
+                return False, []
+        # (Yet) unknown
+        else:
+            raise ValueError(
+                f"Unknown CV structure for element: {el} and value: {val}."
+            )
+
+    def _compare_CV(self, dic2comp, errmsg_prefix):
+        """Compares dictionary of key-val pairs with CV."""
+        checked = {key: False for key in dic2comp.keys()}
+        messages = []
+        for attr in dic2comp.keys():
+            if self.debug:
+                print(attr)
+            if attr in self.CV:
+                if self.debug:
+                    print(attr, "1st level")
+                errmsg = f"""{errmsg_prefix}'{attr}' does not comply with the CV: '{dic2comp[attr] if dic2comp[attr] else 'unset'}'."""
+                checked[attr] = True
+                test, attrs_lvl2 = self._compare_CV_element(
+                    self.CV[attr], dic2comp[attr]
+                )
+                # If comparison fails
+                if not test:
+                    messages.append(errmsg)
+                # If comparison could not be processed completely, as the CV element is another dictionary
+                else:
+                    for attr_lvl2 in attrs_lvl2:
+                        if attr_lvl2 in dic2comp.keys():
+                            if self.debug:
+                                print(attr, "2nd level")
+                            errmsg_lvl2 = f"""{errmsg_prefix}'{attr_lvl2}' does not comply with the CV: '{dic2comp[attr_lvl2] if dic2comp[attr_lvl2] else 'unset'}'."""
+                            checked[attr_lvl2] = True
+                            try:
+                                test, attrs_lvl3 = self._compare_CV_element(
+                                    self.CV[attr][dic2comp[attr]][attr_lvl2],
+                                    dic2comp[attr_lvl2],
+                                )
+                            except ValueError:
+                                raise ValueError(
+                                    f"Unknown CV structure for element {attr} -> {self.CV[attr][dic2comp[attr]][attr_lvl2]} / {attr_lvl2} -> {dic2comp[attr_lvl2]}."
+                                )
+                            if not test:
+                                messages.append(errmsg_lvl2)
+                            else:
+                                if len(attrs_lvl3) > 0:
+                                    raise ValueError(
+                                        f"Unknown CV structure for element {attr} -> {dic2comp[attr]} -> {attr_lvl2}."
+                                    )
+        return checked, messages
+
+    def _map_drs_blocks(self):
+        """Maps the file metadata, name and location to the DRS building blocks and required attributes."""
+        try:
+            drs_path_template = re.findall(
+                r"<([^<>]*)\>", self.CV["DRS"]["directory_path_template"]
+            )
+            drs_filename_template = re.findall(
+                r"<([^<>]*)\>", self.CV["DRS"]["filename_template"]
+            )
+            self.drs_suffix = ".".join(
+                self.CV["DRS"]["filename_template"].split(".")[1:]
+            )
+        except KeyError:
+            raise KeyError("The CV does not contain DRS information.")
+
+        # Map DRS path elements
+        self.drs_dir = {}
+        fps = os.path.dirname(
+            os.path.normpath(os.path.realpath(self.dataset.filepath()))
+        ).split(os.sep)
+        for i in range(-1, -len(drs_path_template) - 1, -1):
+            try:
+                self.drs_dir[drs_path_template[i]] = fps[i]
+            except IndexError:
+                self.drs_dir[drs_path_template[i]] = False
+
+        # Map DRS filename elements
+        self.drs_fn = {}
+        fns = os.path.basename(self.dataset.filepath()).split(".")[0].split("_")
+        for i in range(len(drs_filename_template)):
+            try:
+                self.drs_fn[drs_filename_template[i]] = fns[i]
+            except IndexError:
+                self.drs_fn[drs_filename_template[i]] = False
+
+        # Map DRS global attributes
+        self.drs_gatts = {}
+        for gatt in self.CV["required_global_attributes"]:
+            if gatt in drs_path_template or gatt in drs_filename_template:
+                try:
+                    self.drs_gatts[gatt] = self.dataset.getncattr(gatt)
+                except AttributeError:
+                    self.drs_gatts[gatt] = False
+
+    def check_drs_CV(self, ds):
+        """DRS building blocks in filename and path checked against CV"""
+        desc = "DRS (CV)"
+        level = BaseCheck.HIGH
+        out_of = 3
+        score = 0
+        messages = []
+
+        # File suffix
+        suffix = ".".join(os.path.basename(ds.filepath()).split(".")[1:])
+        if self.drs_suffix == suffix:
+            score += 1
+        else:
+            messages.append(
+                f"File suffix differs from expectation ('{self.drs_suffix}'): '{suffix}'."
+            )
+
+        # DRS path
+        drs_dir_checked, drs_dir_messages = self._compare_CV(
+            self.drs_dir, "DRS path building block "
+        )
+        if len(drs_dir_messages) == 0:
+            score += 1
+        else:
+            messages.extend(drs_dir_messages)
+
+        # DRS filename
+        drs_fn_checked, drs_fn_messages = self._compare_CV(
+            self.drs_fn, "DRS filename building block "
+        )
+        if len(drs_fn_messages) == 0:
+            score += 1
+        else:
+            messages.extend(drs_fn_messages)
+
+        # Unchecked DRS path building blocks
+        unchecked = [key for key in self.drs_dir.keys() if not drs_dir_checked[key]]
+        if len(unchecked) == 0:
+            score += 1
+        else:
+            messages.append(
+                f"""DRS path building blocks could not be checked: {', '.join("'{0}'".format(ukey) for ukey in unchecked)}."""
+            )
+
+        # Unchecked DRS filename building blocks
+        unchecked = [key for key in self.drs_fn.keys() if not drs_fn_checked[key]]
+        if len(unchecked) == 0:
+            score += 1
+        else:
+            messages.append(
+                f"""DRS filename building blocks could not be checked: {', '.join("'{0}'".format(ukey) for ukey in unchecked)}."""
+            )
+
+        return self.make_result(level, score, out_of, desc, messages)
+
+    def check_drs_consistency(self, ds):
+        """DRS building blocks in filename, path and global attributes checked for consistency"""
+        desc = "DRS (consistency)"
+        level = BaseCheck.HIGH
+        out_of = 1
+        score = 0
+        messages = []
+
+        # Union of all DRS building blocks
+        blocks = sorted(
+            list(
+                set(self.drs_gatts.keys()).union(
+                    set(self.drs_fn.keys()).union(set(self.drs_dir.keys()))
+                )
+            )
+        )
+        flaw = False
+        # Check if the values for the DRS building blocks are consistent
+        for att in blocks:
+            atts = {
+                "file path": self.drs_dir.get(att, False),
+                "file name": self.drs_fn.get(att, False),
+                "global attributes": self.drs_gatts.get(att, False),
+            }
+            if len({x for x in atts.values() if x}) > 1:
+                messages.append(
+                    f"""Value for DRS building block '{att}' is not consistent between {" and ".join(["'"+key+"'" for key in sorted(list(atts.keys())) if atts[key]])}: {" and ".join(["'"+atts[key]+"'" for key in sorted(list(atts.keys())) if atts[key]])}."""
+                )
+                flaw = True
+        if not flaw:
+            score += 1
+
+        return self.make_result(level, score, out_of, desc, messages)
 
     def check_format(self, ds):
         """Checks if the file is in the expected format."""
